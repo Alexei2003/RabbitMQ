5cbb049ad251e43153cd20ad8c0c8211b1b5f973
Aliaksei Dubouski <91179353+Alexei2003@users.noreply.github.com>
2024-04-19 18:58:17 +0300

    Initial commit

---
+++ Get/Get.cs
@@ -0,0 +1,31 @@
+﻿using RabbitMQ;
+
+internal class Get
+{
+    //Get
+    private static void Main(string[] args)
+    {
+        var rabbitMQConsumer0 = new RabbitMQConsumer("192.168.148.103", 5672, "Web", "Web", "DataLine0");
+        var rabbitMQConsumer1 = new RabbitMQConsumer("192.168.148.103", 5672, "Web", "Web", "DataLine1");
+        var rabbitMQConsumerDef = new RabbitMQConsumer("192.168.148.103", 5672, "Web", "Web", "DataLineDef");
+
+        var messages = new string[3];
+
+        while (true)
+        {
+            messages[0] = rabbitMQConsumer0.GetMessage();
+            messages[1] = rabbitMQConsumer1.GetMessage();
+            messages[2] = rabbitMQConsumerDef.GetMessage();
+            foreach (var message in messages)
+            {
+                if (message != null)
+                {
+                    Console.WriteLine(message);
+                }
+
+            }
+
+            Thread.Sleep(100);
+        }
+    }
+}
---
+++ Send/Send.cs
@@ -0,0 +1,40 @@
+﻿using RabbitMQ;
+
+internal class Send
+{
+    //Send
+    private static void Main(string[] args)
+    {
+        var rabbitMQPublisher0 = new RabbitMQPublisher("192.168.148.103", 5672, "Web", "Web", "DataLine0");
+        var rabbitMQPublisher1 = new RabbitMQPublisher("192.168.148.103", 5672, "Web", "Web", "DataLine1");
+        var rabbitMQPublisherDef = new RabbitMQPublisher("192.168.148.103", 5672, "Web", "Web", "DataLineDef");
+
+        string message;
+
+        while (true)
+        {
+            message = Console.ReadLine();
+            
+            if(message != "")
+            {
+                switch (message.First())
+                {
+                    case '0':
+                        rabbitMQPublisher0.SendMessage(message);
+                        break;
+                    case '1':
+                        rabbitMQPublisher1.SendMessage(message);
+                        break;
+                    default:
+                        rabbitMQPublisherDef.SendMessage(message);
+                        break;
+                }
+            }
+            else
+            {
+                rabbitMQPublisherDef.SendMessage(message);
+            }
+
+        }
+    }
+}
